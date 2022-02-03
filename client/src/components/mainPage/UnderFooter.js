534110a23297cdc2ed9a2d5c99a2139819dd957c
mindyGit <mindysahar@gmail.com>
2022-02-03 11:25:51 +0200

    02/02/2022 commit

--- client/src/components/mainPage/UnderFooter.js
+++ client/src/components/mainPage/UnderFooter.js
@@ -4,11 +4,11 @@ import { Container, Form, FormControl, Nav, Button, NavDropdown, Image } from "r
 export function UnderFooter() {
     return (
         <footer style={{ backgroundColor: '#403F3F' }}>
-            <div class="row pt-2 pb-2 col-md-12 justify-content-between  align-items-center whiteColor mr-0" >
+            <div className="row pt-2 pb-2 col-md-12 justify-content-between  align-items-center whiteColor mr-0" >
 
-                <div class="col-md-2 h6 mb-0 ">תקנון האתר</div>
-                <div class="col-md-3 h6 mb-0"> כל הזכויות שמורות לסקופ קייטרינג - scoop</div>
-                <div class="col-md-2 h6 mb-0 " style={{ color: "#C59950" }}>ui | ux</div>
+                <div className="col-md-2 h6 mb-0 ">תקנון האתר</div>
+                <div className="col-md-3 h6 mb-0"> כל הזכויות שמורות לסקופ קייטרינג - scoop</div>
+                <div className="col-md-2 h6 mb-0 " style={{ color: "#C59950" }}>ui | ux</div>
 
 
 
